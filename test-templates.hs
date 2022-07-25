f0ec24115694f122af0ee5e48c2b386077adb68e
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2022-07-25 23:39:05 +0100

    Complete update of test-templates.hs

--- test-templates.hs
+++ test-templates.hs
@@ -38,14 +38,14 @@ import           WithCli
 
 excluded :: [String]
 excluded =
---  "foundation" : -- foundation package is not available on stackage
   "ghcjs-old-base" : -- ghcjs takes too long to setup
   "ghcjs" : -- ghcjs takes too long to setup
   "hakyll-template" : -- hakyll takes an excessive amount of time to compile its dependencies
   "quickcheck-test-framework" : -- test-suite fails (probably intentionally)
---  "simple-hpack" : -- stack init fails on missing cabal file (fixed in stack on master)
   "spock" : -- Spock not in LTS since lts-12.26 (GHC 8.4.4)
   "tasty-discover" : -- contains a stack file, makes `stack new` choke
+  "tasty-travis" : -- the two `prop>` in `*-doctest` test suite fail. See
+                   -- https://github.com/sol/doctest/issues/202#issuecomment-1194695852
   "yesod-mongo" : -- needs a running db instance
   "yesod-mysql" : -- needs a running db instance
   "yesod-postgres-fay" : -- needs a running db instance
